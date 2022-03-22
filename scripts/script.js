eac2f532cb936f4b19d860f71dce00fe2761a8be
Tomas <tomas.huzevka@outlook.com>
2022-03-22 06:56:35 -0400

    added functioning search bar

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,64 @@ const myChart = new Chart(ctx, {
         }
     }
 });
+
+// Search Bar 
+import { secret_key } from "./modules/api_keys.js"
+let movieData = [];
+
+// Button Listener
+document.getElementById("searchMovieForm").addEventListener("submit", searchMovieButtonPress);
+
+function searchMovieButtonPress(evt) {
+    evt.preventDefault();
+    
+    // console.log(evt);
+    // user input variables
+    const movie_title_input = evt.target.elements[1].value.replace(/ /g,"+");;
+    const movie_release_year = evt.target.elements[2].value;
+
+    getMovieData({
+        movie_title_input, 
+        movie_release_year});
+}
+
+function getMovieData({movie_title_input, movie_release_year}) {
+    // http://www.omdbapi.com/?t=indiana+jones&apikey=44662e28
+    console.log(movie_title_input, movie_release_year)
+    console.log((movie_release_year.length < 1))
+    let omdb_link;
+
+    if (movie_release_year.length < 1){
+        omdb_link = `http://www.omdbapi.com/?t=${movie_title_input}&apikey=${secret_key}`;
+    }
+    else {
+        omdb_link = `http://www.omdbapi.com/?t=${movie_title_input}&y=${movie_release_year}&apikey=${secret_key}`;
+    }
+
+    fetch(omdb_link)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function(data){
+        movieCardData(data);
+    })
+}
+
+function movieCardData(data){
+    movieData.push(data)
+    // will delete
+    justPrint();
+}
+
+// Will delete this. This is just to get data more easier
+function justPrint(){
+    // gets last move
+    let index = (movieData.length - 1);
+
+    console.log(movieData[index].Title)
+    console.log(movieData[index].Poster)
+    console.log(movieData[index].Genre)
+    console.log(movieData[index].imdbRating)
+    console.log(movieData[index].Actors)
+    console.log(movieData[index].Released)
+}
