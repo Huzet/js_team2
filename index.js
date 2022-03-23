32204a5b17bfe899ca6252c0b69aee60f8a110b8
Tomas <tomas.huzevka@outlook.com>
2022-03-23 14:12:35 -0400

    fixed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const fetch = require("node-fetch");
 const cors = require('cors')
 const server = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const fs = require('fs');
 
 
